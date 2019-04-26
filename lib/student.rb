7d4e00fc7f21d5ded714f7c421165d11941bcab5
Tracy Ngo <tngo1206@gmail.com>
2019-04-26 03:28:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,40 @@
+require 'pry'
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = Student.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql_all = <<-SQL
+      SELECT * FROM students
+    SQL
+    
+    DB[:conn].execute(sql_all).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql_find = <<-SQL
+      SELECT * FROM students WHERE name = ?
+      LIMIT 1
+      SQL
+    
+      DB[:conn].execute(sql_find, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  
   end
   
   def save
@@ -40,4 +62,35 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  def self.all_students_in_grade_9
+    sql_query = <<-SQL
+    SELECT * FROM students WHERE grade = 9
+    SQL
+    
+    DB[:conn].execute(sql_query).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.students_below_12th_grade
+    sql_query= <<-SQL
+    SELECT * FROM students WHERE grade < 12
+    SQL
+    
+    DB[:conn].execute(sql_query).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.first_X_students_in_grade_10
+    sql_query = <<-SQL
+    SELECT * FROM students WHERE grade < 12
+    SQL
+    
+    DB[:conn].execute(sql_grade_lessthan_12).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
 end
