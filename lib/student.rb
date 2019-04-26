b4b6ef5077dbe78203534e794855708e2cac723b
Tracy Ngo <tngo1206@gmail.com>
2019-04-26 03:34:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -93,4 +93,15 @@ class Student
     end
   end
   
+   def self.first_student_in_grade_10(num)
+    sql_query = <<-SQL
+    SELECT * FROM students WHERE grade = 10
+    LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql_query, num).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
 end
