acde21e833eb374ddb557ca809aff12f4b51e056
Tracy Ngo <tngo1206@gmail.com>
2019-04-26 03:32:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,7 +82,7 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(num)
     sql_query = <<-SQL
     SELECT * FROM students WHERE grade < 12
     SQL
